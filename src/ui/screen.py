7b86d23367d807e91037f5aea3640ffb84a92cad
Alberto Sanchez <alberto.sanchez.delalamo@gmail.com>
2023-04-16 20:55:24 +0200

    no radio buttons

--- src/ui/screen.py
+++ src/ui/screen.py
@@ -15,30 +15,24 @@ class Screen:
         self.label = Label(self.root, image=self.logo)
         self.label.pack(padx=40)
 
-        self.label2 = Label(self.root, text="Introduce the URL", font=("Arial", 11))
+        self.label2 = Label(self.root, text="Please insert a valid YouTube video URL", 
+                            font=("Arial", 11))
         self.label2.pack()
-        self.entry1 = Entry(self.root, width=40, highlightcolor="red", 
+        self.url = Entry(self.root, width=40, highlightcolor="red", 
                             highlightthickness=2, font=("Roboto", 11))
-        self.entry1.pack()
+        self.url.pack()
 
-        self.label3 = Label(self.root, text="Introduce the download path", 
-                            font=("Roboto", 11))
-        self.label3.pack()
-        self.boton1 = Button(self.root, text="Download path", bg="#F9E79F", 
-                             padx=5, pady=5, font=("Roboto", 11))
-        self.boton1.pack()
-
-        self.boton2 = Button(self.root, text="Download", bg="#F9E79F", padx=5, pady=5,
+        self.download_button = Button(self.root, text="Download", bg="#F9E79F", padx=5, pady=5,
                         font=("Roboto", 11), command=self.initialize_download)
-        self.boton2.pack(pady=20)
+        self.download_button.pack(pady=20)
 
-        self.btn = RoundedButton(text="Download", radius=30, width=100, height=50,
+        """self.btn = RoundedButton(text="Download", radius=30, width=100, height=50,
                             btnbackground="#0078ff", btnforeground="#ffffff")
-        self.btn.pack(padx=20, pady=20)
+        self.btn.pack(padx=20, pady=20)"""
+
+    def initialize_download(self):
+        link = self.url.get()
+        Video(link, "video").download()
 
     def execute(self):
         self.root.mainloop()
-    
-    def initialize_download(self):
-        link = self.entry1.get()
-        Video(link, "video").download()
